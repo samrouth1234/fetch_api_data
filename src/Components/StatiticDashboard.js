0230807826537be3d1d3e88d24bee617428e7e08
samrouth <bichsamroth@gmail.com>
2023-07-02 17:11:58 +0700

    update table fetch data

--- src/Components/StatiticDashboard.js
+++ src/Components/StatiticDashboard.js
@@ -1,48 +1,69 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import DataTable from 'react-data-table-component';
+
 export default function StatiticDashboard() {
-      const [data, setData] = useState([]);
+      const [certificateData, setCertificateData] = useState([]);
+      const [userData, setUserData] = useState([]);
       const [isLoading, setLoading] = useState(true);
+
       useEffect(() => {
             setLoading(true);
-            const fetchData = async () => {
+            const fetchCertificateData = async () => {
                   try {
                         const response = await fetch("http://136.228.158.126:8002/api/v1/certificates?page=1&limit=20");
                         const responseData = await response.json();
-                        const dataRes = await responseData.data;
-                        const list = await dataRes.list;
-                        console.log("list: ", list);
-                        setData(list);
+                        const data = await responseData.data.list;
+                        console.log("list Certificate: ", data);
+                        setCertificateData(data);
                   } catch (error) {
                         console.error("error: " + error);
                   }
             };
-            fetchData();
+            fetchCertificateData();
       }, []);
+
+      useEffect(() => {
+            setLoading(true);
+            const fetchUserData = async () => {
+                  try {
+                        const response = await fetch("http://136.228.158.126:8002/api/v1/users");
+                        const responseData = await response.json();
+                        const data = await responseData.data.list;
+                        console.log("list User: ", data);
+                        setUserData(data);
+                  } catch (error) {
+                        console.error("error: " + error);
+                  }
+            };
+            fetchUserData();
+      }, []);
+
+      const data = certificateData.concat(userData);
+
       const columns = [
             {
-                name: 'Number of Created Certificate',
-                selector: 'createdBy',
+                  name: 'Number of Created Certificate',
+                  selector: 'id',
             },
             {
-                name: 'User',
-                selector: '',
+                  name: 'User',
+                  selector: 'username',
             },
             {
-                name: 'Date',
-                selector: 'createdAt'
+                  name: 'Date',
+                  selector: 'createdAt'
             },
-        ];
+      ];
 
       return (
             <div className='container mt-5'>
-            <DataTable
-                title="Product"
-                columns={columns}
-                data={data}
-                pagination
-            />
-        </div>
+                  <DataTable
+                        title="Product"
+                        columns={columns}
+                        data={data}
+                        pagination
+                  />
+            </div>
       )
 }
